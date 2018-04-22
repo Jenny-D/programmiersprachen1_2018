169a22f50ad3ff10d6ae5f8df30d87599e59c08b
Jenny-D <38217695+Jenny-D@users.noreply.github.com>
2018-04-22 17:18:13 +0200

    gcd

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,5 +1,25 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
+#include <cmath>
+
+int gcd(int a, int b)
+{
+  return 1;
+}
+
+int describe_gcd(int a, int b)
+{
+  if (b == 0)
+    return a;
+  else return describe_gcd(b, a%b);
+}
+
+TEST_CASE("describe_gcd", "[gcd]")
+{
+  REQUIRE(gcd(2,4) == 2);
+  REQUIRE(gcd(9,6) == 3);
+  REQUIRE(gcd(3,7) == 1);
+}
 
 int main(int argc, char* argv[])
 {
