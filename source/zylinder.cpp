e0d266415178609778a203dcc3e5989792a994a6
Jenny-D <38217695+Jenny-D@users.noreply.github.com>
2018-04-23 13:30:49 +0200

    update

--- source/zylinder.cpp
+++ source/zylinder.cpp
@@ -2,9 +2,34 @@
 #include <catch.hpp>
 #include <cmath>
 
+double oberflaeche_zylinder(double radius, double hoehe)
+{
+	double oberflaeche;
+	if(radius >= 0 && hoehe >= 0){
+		oberflaeche = 2 * M_PI * radius * (radius + hoehe);
+	}
+	else{
+		oberflaeche = 0;
+	}
+	return oberflaeche;
+}
+
 double volumen_zylinder(double radius, double hoehe)
 {
-	
+	double volumen;
+	if (radius >= 0 && hoehe >= 0){
+		volumen = M_PI * pow(radius, 2) * hoehe;
+	}
+	else{
+		volumen = 0;
+	}
+	return volumen;
+}
+
+TEST_CASE("testing_oberflaeche_zylinder", "[oberflaeche_zylinder]")
+{
+  REQUIRE(oberflaeche_zylinder(2.0,3.0) == 62,83);
+  REQUIRE(oberflaeche_zylinder(1.0,2.0) == 18,849);
 }
 
 TEST_CASE("testing_volumen_zylinder", "[volumen_zylinder]")
